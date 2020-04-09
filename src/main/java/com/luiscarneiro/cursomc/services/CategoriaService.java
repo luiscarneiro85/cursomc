274ff04e6312e44a41c7193143b6afae2ef862b8
Luis Carneiro <luifecarneiro@gmail.com>
2020-04-09 14:52:54 -0300

    Created Repository and Service for Categoria

---
+++ src/main/java/com/luiscarneiro/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.luiscarneiro.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luiscarneiro.cursomc.domain.Categoria;
+import com.luiscarneiro.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+}
