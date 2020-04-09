f180a34bbe638b3f711954349ee7108bb9510afd
Luis Carneiro <luifecarneiro@gmail.com>
2020-04-09 16:25:53 -0300

    Produto e associação ManyToMany

---
+++ src/main/java/com/luiscarneiro/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.luiscarneiro.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luiscarneiro.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
