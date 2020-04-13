d7a88d3ee55afca9b469f5d3f03aca1b4ddfc86c
Luis Carneiro <luifecarneiro@gmail.com>
2020-04-13 14:58:30 -0300

    Deletentado uma categoria com DELETE

--- src/main/java/com/luiscarneiro/cursomc/services/CategoriaService.java
+++ src/main/java/com/luiscarneiro/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.luiscarneiro.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.luiscarneiro.cursomc.domain.Categoria;
 import com.luiscarneiro.cursomc.repositories.CategoriaRepository;
+import com.luiscarneiro.cursomc.services.exceptions.DataIntegrityException;
 import com.luiscarneiro.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,5 +33,15 @@ public class CategoriaService {
 		find(obj.getId());
 		
 		return repository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos.");
+		}		
 	}
 }
