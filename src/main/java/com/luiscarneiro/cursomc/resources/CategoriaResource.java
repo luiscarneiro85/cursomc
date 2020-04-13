028a2f577700a986f13f888b410eeb1b342d1280
Luis Carneiro <luifecarneiro@gmail.com>
2020-04-13 15:12:43 -0300

    listando todas categorias com DTO

--- src/main/java/com/luiscarneiro/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/luiscarneiro/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.luiscarneiro.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luiscarneiro.cursomc.domain.Categoria;
+import com.luiscarneiro.cursomc.dto.CategoriaDTO;
 import com.luiscarneiro.cursomc.services.CategoriaService;
 
 @RestController
@@ -48,5 +51,16 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		
+		//Converte uma lista de um tipo de objeto em outra lista
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj))
+				.collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
