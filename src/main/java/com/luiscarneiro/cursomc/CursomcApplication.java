3d2fb6fcdabd6e54b053d9eef1acbfeadc47649a
Luis Carneiro <luifecarneiro@gmail.com>
2020-04-12 23:46:53 -0300

    Created ItemPedido and ItemPedidoPK

--- src/main/java/com/luiscarneiro/cursomc/CursomcApplication.java
+++ src/main/java/com/luiscarneiro/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.luiscarneiro.cursomc.domain.Cidade;
 import com.luiscarneiro.cursomc.domain.Cliente;
 import com.luiscarneiro.cursomc.domain.Endereco;
 import com.luiscarneiro.cursomc.domain.Estado;
+import com.luiscarneiro.cursomc.domain.ItemPedido;
 import com.luiscarneiro.cursomc.domain.Pagamento;
 import com.luiscarneiro.cursomc.domain.PagamentoComBoleto;
 import com.luiscarneiro.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.luiscarneiro.cursomc.repositories.CidadeRepository;
 import com.luiscarneiro.cursomc.repositories.ClienteRepository;
 import com.luiscarneiro.cursomc.repositories.EnderecoRepository;
 import com.luiscarneiro.cursomc.repositories.EstadoRepository;
+import com.luiscarneiro.cursomc.repositories.ItemPedidoRepository;
 import com.luiscarneiro.cursomc.repositories.PagamentoRepository;
 import com.luiscarneiro.cursomc.repositories.PedidoRepository;
 import com.luiscarneiro.cursomc.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursomcApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired 
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -111,5 +115,18 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto2, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 }
---
+++ src/main/java/com/luiscarneiro/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.luiscarneiro.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luiscarneiro.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
