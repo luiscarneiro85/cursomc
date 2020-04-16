0d7dd0bc0e024ad62a4a71f0b2f270565658174e
Luis Carneiro <luifecarneiro@gmail.com>
2020-04-15 23:19:18 -0300

    Validacao customizada no Update do Cliente

--- src/main/java/com/luiscarneiro/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/luiscarneiro/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.luiscarneiro.cursomc.domain.Cliente;
+import com.luiscarneiro.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
