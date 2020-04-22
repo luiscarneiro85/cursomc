7b13d7663325f7f24cb0add16070c4d17294fbd3
Luis Carneiro <luifecarneiro@gmail.com>
2020-04-22 01:04:29 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/luiscarneiro/cursomc/config/DevConfig.java
+++ src/main/java/com/luiscarneiro/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.luiscarneiro.cursomc.services.DBService;
+import com.luiscarneiro.cursomc.services.EmailService;
+import com.luiscarneiro.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 
 		dbService.instantiateTestDataBase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
