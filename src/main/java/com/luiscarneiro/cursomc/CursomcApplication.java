5d76eb464f12921c98b1a724994a250a3b803bc5
Luis Carneiro <luifecarneiro@gmail.com>
2020-05-03 01:00:26 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/luiscarneiro/cursomc/CursomcApplication.java
+++ src/main/java/com/luiscarneiro/cursomc/CursomcApplication.java
@@ -1,35 +1,8 @@
 package com.luiscarneiro.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.luiscarneiro.cursomc.domain.Categoria;
-import com.luiscarneiro.cursomc.domain.Cidade;
-import com.luiscarneiro.cursomc.domain.Cliente;
-import com.luiscarneiro.cursomc.domain.Endereco;
-import com.luiscarneiro.cursomc.domain.Estado;
-import com.luiscarneiro.cursomc.domain.ItemPedido;
-import com.luiscarneiro.cursomc.domain.Pagamento;
-import com.luiscarneiro.cursomc.domain.PagamentoComBoleto;
-import com.luiscarneiro.cursomc.domain.PagamentoComCartao;
-import com.luiscarneiro.cursomc.domain.Pedido;
-import com.luiscarneiro.cursomc.domain.Produto;
-import com.luiscarneiro.cursomc.domain.enums.EstadoPagamento;
-import com.luiscarneiro.cursomc.domain.enums.TipoCliente;
-import com.luiscarneiro.cursomc.repositories.CategoriaRepository;
-import com.luiscarneiro.cursomc.repositories.CidadeRepository;
-import com.luiscarneiro.cursomc.repositories.ClienteRepository;
-import com.luiscarneiro.cursomc.repositories.EnderecoRepository;
-import com.luiscarneiro.cursomc.repositories.EstadoRepository;
-import com.luiscarneiro.cursomc.repositories.ItemPedidoRepository;
-import com.luiscarneiro.cursomc.repositories.PagamentoRepository;
-import com.luiscarneiro.cursomc.repositories.PedidoRepository;
-import com.luiscarneiro.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
